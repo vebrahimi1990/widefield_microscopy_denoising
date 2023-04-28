fedbf80946d4486b2fafe6edff4cb9ebd1a832c8
Vahid Ebrahimi <95882710+vebrahimi1990@users.noreply.github.com>
2023-04-28 16:42:25 -0400

    Plot_data: Plotting a sample of dataset.

--- three_dim/train.py
+++ three_dim/train.py
@@ -7,6 +7,8 @@ from config import CFG
 from datagenerator import data_generator_3D
 from loss import loss
 from model import UNet, UNet_RCAN, RCAN
+import matplotlib.pyplot as plt
+import numpy as np
 
 gpus = tf.config.list_physical_devices('GPU')
 tf.config.set_logical_device_configuration(gpus[0], [tf.config.LogicalDeviceConfiguration(memory_limit=12000)])
@@ -17,7 +19,28 @@ model_config = CFG['model']
 callback = CFG['callbacks']
 x_train, y_train = data_generator_3D(data_config)
 
-model_input = Input((data_config['patch_size'], data_config['patch_size'],data_config['fr_end']-data_config['fr_start'], 1))
+mip_x_train = np.max(x_train, axis=3)
+mip_y_train = np.max(y_train, axis=3)
+
+ix = np.random.randint(0, len(x_train), 4)
+fig = plt.figure(figsize=(15, 7))
+
+for i in range(4):
+    fig.add_subplot(2, 4, 2 * i + 1)
+    cmap = plt.get_cmap('magma')
+    plt.imshow(mip_x_train[ix[i], :, :, 0].squeeze(), cmap)
+    plt.title('Low SNR', fontdict={'fontsize': 18})
+    plt_axis = plt.axis('off')
+
+    fig.add_subplot(2, 4, 2 * i + 2)
+    cmap = plt.get_cmap('magma')
+    plt.imshow(mip_y_train[ix[i], :, :, 0].squeeze(), cmap)
+    plt.title('High SNR', fontdict={'fontsize': 18})
+    plt_axis = plt.axis('off')
+plt.show()
+
+model_input = Input(
+    (data_config['patch_size'], data_config['patch_size'], data_config['fr_end'] - data_config['fr_start'], 1))
 model = eval(model_config['model_type'] + "(model_input, model_config)")
 print(model.summary())
 
